2e6ef5f162f1831b2ba853706a926227c3871ce4
GitHaberr <drondaronght@gmail.com>
2026-06-11 00:06:41 +0700

    Add Models, Student, Subject, Grade

--- students/models.py
+++ students/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.db.models import Avg
 
-# Create your models here.
+class Student(models.Model):
+    student_id = models.CharField(max_length=20)
+    full_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.full_name
+
+    def average_score(self):
+        avg = self.grade_set.aggregate(Avg('grade'))['grade__avg']
+        return avg if avg else 0
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Grade(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    grade = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.student.full_name} - {self.subject.name}: {self.grade}"
