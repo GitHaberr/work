377933d89d701da5cbdc41c2e5118fae119eeeec
GitHaberr <drondaronght@gmail.com>
2026-06-11 14:22:50 +0700

    Edit index.html and views.py

--- students/views.py
+++ students/views.py
@@ -1,4 +1,34 @@
-from django.shortcuts import render
+# views.py
+from collections import defaultdict
+from django.views.generic.base import TemplateView
+from .models import Grade
 
-def index_page(request):
-    return render(request, 'index.html')
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        grades = Grade.objects.all()
+        student_grades = defaultdict(dict)
+
+        subjects = set()
+        for grade in grades:
+            subject_name = grade.subject.name
+            subjects.add(subject_name)
+            student_grades[grade.student][subject_name] = grade.value
+
+        subjects = sorted(subjects)
+        student_statistics = [
+            {
+                'student': student,
+                'scores': [f'{grades[subject]:.1f}' for subject in subjects]
+            }
+            for student, grades in student_grades.items()
+        ]
+        context.update(
+            {
+                'subjects': subjects,
+                'student_statistics': student_statistics
+            }
+        )
+        return context
